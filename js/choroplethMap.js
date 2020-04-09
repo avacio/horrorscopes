107e1d68fbda2a70bdfaad6e7a7171d4ee8bf5cc
Margaret Yao <>
2020-04-09 15:46:15 -0700

    removing comments, reducing opacity for non selected countries

--- js/choroplethMap.js
+++ js/choroplethMap.js
@@ -180,31 +180,14 @@ class ChoroplethMap {
       .transition()
         .attr('fill', d => {
           return vis.colorScale(vis.killersByCountry[d.properties.name]);
-
-/*
-          if (vis.active._parents[0] == null) {
-            return vis.colorScale(vis.killersByCountry[d.properties.name]);
-          } else {
-            return 'blue';
-          }
-          
-
-          /*
-          if (vis.killersByCountry[d.properties.name] != 0) {
-            return '#3b5d38';
-          } else {
-            return '#D3D3D3';
-          }*/
-          // To-do: Change fill to color code each province by its population
         })
         .attr('opacity', d => {
-          console.log(d);
            //console.log(selectedCountry);
             if (selectedCountry != null) {
               if (d.properties.name == selectedCountry) {
                 return '1';
               } else {
-                return '0.5';
+                return '0.2';
               }
             }
         })
@@ -237,85 +220,3 @@ class ChoroplethMap {
       });  */
   }
 }
-
-
-
-
-/*
-class ChoroplethMap {
-
-  constructor(_config) {
-    this.config = {
-      parentElement: _config.parentElement,
-      containerWidth: _config.containerWidth || 1000,
-      containerHeight: _config.containerHeight || 600,
-    }
-
-    this.year = _config.year;
-
-    this.initVis();
-  }
-
-  initVis() {
-    let vis = this;
-
-    vis.svg = d3.select(vis.config.parentElement)
-      .attr('width', vis.config.containerWidth)
-      .attr('height', vis.config.containerHeight);
-
-    vis.chart = vis.svg.append('g')
-      .attr('transform', 'translate(20,400), scale(0.8,0.8)');
-
-    // We initialize a geographic path generator, that is similar to shape generators that you have used before (e.g. d3.line())
-    // We define a projection: https://github.com/d3/d3-geo/blob/v1.11.9/README.md#geoAlbers
-    vis.path = d3.geoPath().projection(d3.geoAlbers());
-  }
-
-  update() {
-    let vis = this;
-
-    // To-do: Add color scale
-    let colorPalette = d3.schemeReds[9];
-
-    this.colorScale = d3.scaleQuantile()
-      .domain(this.domain)
-      .range(colorPalette);
-
-    this.curr_data = this.population.find(d => d.year == this.year);
-
-    vis.render();
-  }
-
-  render() {
-    let vis = this;
-
-    // 
-    let geoPath = vis.chart.selectAll('.geo-path')
-    .data(topojson.feature(vis.canada_geo, vis.canada_geo.objects.provinces).features);
-
-    let geoPathEnter = geoPath.enter().append('path')
-    .attr('class', 'geo-path')
-    .attr("d", vis.path);
-
-    geoPath.merge(geoPathEnter)
-      .transition()
-      .attr('fill', d => {
-      // To-do: Change fill to color code each province by its population
-      return this.colorScale(this.curr_data[d.id]);
-    });
-
-    // To-do: Add labels for each province with the population value
-
-    // draw labels
-    vis.chart
-      .selectAll("text")
-      .data(topojson.feature(vis.canada_geo, vis.canada_geo.objects.provinces).features)
-      .join("text")
-      .attr("class", "place-text")
-      .attr("x", d => { return vis.path.centroid(d)[0]; })
-      .attr("y", d => { return vis.path.centroid(d)[1]; })
-      .attr("text-anchor","middle")
-      .text(d => d3.format(".2s")(this.curr_data[d.id]));
-  }
-}
-*/
